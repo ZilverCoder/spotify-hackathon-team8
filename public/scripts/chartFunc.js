31c69d26982293c92bae765c64ffcd7e56154a35
ZilverCoder <zilvergamer@gmail.com>
2018-10-13 18:30:51 +0200

    :factory: Rearranged chart functions

--- public/scripts/chartFunc.js
+++ public/scripts/chartFunc.js
@@ -1,72 +1,3 @@
-//#region Custom Tooltip
-var customTooltips = function(tooltip) {
-	// Tooltip Element
-	var tooltipEl = $('#album')[0];
-	var albumCover = $('.info--cover');
-	var albumTitle = $('.info--title');
-	console.log(tooltip);
-	//IF ELEMENT DOES NOT EXIST
-	/*if (!tooltipEl) {
-		tooltipEl = document.createElement('div');
-		tooltipEl.id = 'album';
-		tooltipEl.innerHTML = '<div></div>';
-		this._chart.canvas.parentNode.appendChild(tooltipEl);
-	}*/
-
-	// Hide if no tooltip
-	if (tooltip.opacity === 0) {
-		tooltipEl.style.opacity = 0;
-		return;
-	}
-
-	// Set caret Position
-	tooltipEl.classList.remove('above', 'below', 'no-transform');
-	if (tooltip.yAlign) {
-		tooltipEl.classList.add(tooltip.yAlign);
-	} else {
-		tooltipEl.classList.add('no-transform');
-	}
-
-	function getBody(bodyItem) {
-		return bodyItem.lines;
-	}
-
-	// Set Text
-	if (tooltip.body) {
-		var titleLines = tooltip.title || [];
-		var bodyLines = tooltip.body.map(getBody);
-
-		titleLines.forEach(function(title) {
-			albumTitle.text('Her');
-		});
-
-		bodyLines.forEach(function(body, i) {
-			/*var colors = tooltip.labelColors[i];
-			var style = 'background:' + colors.backgroundColor;
-			style += '; border-color:' + colors.borderColor;
-			style += '; border-width: 2px';
-			var span = '<span class="chartjs-tooltip-key" style="' + style + '"></span>';
-			innerHtml += '<div>' + span + body + '</div>';*/
-			console.log(body);
-		});
-
-		/*var tableRoot = document.getElementsByClassName('album');
-		tableRoot.innerHTML = innerHtml;*/
-	}
-
-	var positionY = this._chart.canvas.offsetTop;
-	var positionX = this._chart.canvas.offsetLeft;
-
-	// Display, position, and set styles for font
-	tooltipEl.style.opacity = 1;
-	tooltipEl.style.left = positionX + tooltip.caretX - 100 + 'px';
-	tooltipEl.style.top = positionY + tooltip.caretY + 'px';
-	//tooltipEl.style.fontFamily = tooltip._bodyFontFamily;
-	//tooltipEl.style.fontSize = tooltip.bodyFontSize + 'px';
-	//tooltipEl.style.fontStyle = tooltip._bodyFontStyle;
-	//tooltipEl.style.padding = tooltip.yPadding + 'px ' + tooltip.xPadding + 'px';
-};
-//#endregion
 var ctx = document.getElementById("myChart").getContext("2d");
 
 var cloud = new Image();
@@ -90,7 +21,6 @@ var data = $.get(`spotify/get-album-data-for-artist?artistId=0oSGxfWSnnOXhD2fKuz
 		testArray.push(new Album(track.releaseDate, track.images, track.features.valence, track.name));
 		albumForChart.push(new AlbumForChart(track.releaseDate, this.features.valence));
 	});
-	console.table(testArray)
 });
 
 function Album(year, imageObject, attr, title){
@@ -114,6 +44,74 @@ function AlbumForChart(year, attr){
 }
 
 $.when(data).done(function(){
+	//#region Custom Tooltip
+		var customTooltips = function(tooltip) {
+			// Tooltip Element
+			var tooltipEl = $('#album')[0];
+			var albumCover = $('.info--cover');
+			var albumTitle = $('.info--title');
+			//IF ELEMENT DOES NOT EXIST
+			/*if (!tooltipEl) {
+				tooltipEl = document.createElement('div');
+				tooltipEl.id = 'album';
+				tooltipEl.innerHTML = '<div></div>';
+				this._chart.canvas.parentNode.appendChild(tooltipEl);
+			}*/
+
+			// Hide if no tooltip
+			if (tooltip.opacity === 0) {
+				tooltipEl.style.opacity = 0;
+				return;
+			}
+
+			// Set caret Position
+			tooltipEl.classList.remove('above', 'below', 'no-transform');
+			if (tooltip.yAlign) {
+				tooltipEl.classList.add(tooltip.yAlign);
+			} else {
+				tooltipEl.classList.add('no-transform');
+			}
+
+			function getBody(bodyItem) {
+				return bodyItem.lines;
+			}
+
+			// Set Text
+			if (tooltip.body) {
+				var titleLines = tooltip.title || [];
+				var bodyLines = tooltip.body.map(getBody);
+
+				titleLines.forEach(function(title) {
+					albumTitle.text('Her');
+				});
+
+				bodyLines.forEach(function(body, i) {
+					/*var colors = tooltip.labelColors[i];
+					var style = 'background:' + colors.backgroundColor;
+					style += '; border-color:' + colors.borderColor;
+					style += '; border-width: 2px';
+					var span = '<span class="chartjs-tooltip-key" style="' + style + '"></span>';
+					innerHtml += '<div>' + span + body + '</div>';*/
+					//console.log(body);
+				});
+
+				/*var tableRoot = document.getElementsByClassName('album');
+				tableRoot.innerHTML = innerHtml;*/
+			}
+
+			var positionY = this._chart.canvas.offsetTop;
+			var positionX = this._chart.canvas.offsetLeft;
+
+			// Display, position, and set styles for font
+			tooltipEl.style.opacity = 1;
+			tooltipEl.style.left = positionX + tooltip.caretX - 100 + 'px';
+			tooltipEl.style.top = positionY + tooltip.caretY + 'px';
+			//tooltipEl.style.fontFamily = tooltip._bodyFontFamily;
+			//tooltipEl.style.fontSize = tooltip.bodyFontSize + 'px';
+			//tooltipEl.style.fontStyle = tooltip._bodyFontStyle;
+			//tooltipEl.style.padding = tooltip.yPadding + 'px ' + tooltip.xPadding + 'px';
+		};
+	//#endregion
 	albumForChart.sort(function(a, b) {
 		var nameA = a.x;
 		var nameB = b.x;
@@ -125,7 +123,7 @@ $.when(data).done(function(){
 		}
 		return 0;
 	});
-	console.log(testArray[0].imageObj);
+	Chart.defaults.global.defaultFontColor = "#fff";
 	var myChart = new Chart(ctx, {
 		type: 'line',
 		data: {
