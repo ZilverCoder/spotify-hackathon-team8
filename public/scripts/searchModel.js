cd8351dd34c32bd3c213392db963ac491901899a
Jan Eisenmenger <eisenmenger.jan@gmail.com>
2018-10-13 18:13:18 +0200

    kinda merged with search page; still errors

--- public/scripts/searchModel.js
+++ public/scripts/searchModel.js
@@ -1,25 +1,33 @@
 var searchModel = function(){
 	var self = this;
-	function Track(albumName, imageUrl){
-		this.albumName = ko.observable(albumName);
+	function Artist(artistId, artistName, imageUrl){
+		this.artistId = ko.observable(artistId)
+		this.artistName = ko.observable(artistName);
 		this.imageUrl = ko.observable(imageUrl);
-    }
-	self.tracks = ko.observableArray();
+	}
+	
+	self.artists = ko.observableArray();
 
-	self.trackSearch = ko.observable();
-	self.searchTrack = ko.computed(function(){
-		self.tracks([]);
-		/* $.get(`spotify/get-album-data-for-artist?artistId=0oSGxfWSnnOXhD2fKuz2Gy`, function(data) {
+	self.artistSearch = ko.observable("");
+
+
+	self.searchArtist = ko.computed(function(){
+		self.artists([]);
+
+		$.get(`spotify/get-artists-from-name?artistName=${self.artistSearch()}`, function(data) {
 			// "Data" is the object we get from the API. See server.js for the function that returns it.
-			console.group('%cResponse from /search-track', 'color: #F037A5; font-size: large');
+			console.group('%cResponse from /get-artist-from-name', 'color: #F037A5; font-size: large');
 			console.log(data);
 			console.groupEnd();
-			$.each(data, function(index, track){
-				//console.log(track.name, track.images[1]);
-				self.tracks().push(new Track(track.name, track.images[1].url));
+			
+			data.artist.forEach( artist => {
+				if (artist.image.length > 0) {
+					self.artists.push(new Artist(artist.id, artist.name, artist.image[1].url));
+				} else {
+					self.artists.push(new Artist(artist.id, artist.name, "https://upload.wikimedia.org/wikipedia/commons/5/5f/Grey.PNG"));
+				}
 			});
-		}); */
-		return "done";
+		});
 	}, this);
 }
 
