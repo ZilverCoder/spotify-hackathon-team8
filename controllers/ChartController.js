510f8283aea5126812d25cbe56230a37d72b7426
ZilverCoder <zilvergamer@gmail.com>
2018-10-13 20:33:49 +0200

    Added chart route and page, fixed search for artist frontend

--- controllers/ChartController.js
+++ controllers/ChartController.js
@@ -2,7 +2,9 @@ var express = require('express');
 var app = express();
 
 app.get('/', function(req, res, next){
-    res.render('Index');
+	var artistId = req.query.artistId;
+	console.log(artistId);
+    res.render('Chart');
 });
 
 module.exports = app;
--- public/scripts/searchModel.js
+++ public/scripts/searchModel.js
@@ -6,29 +6,27 @@ var searchModel = function(){
 		this.imageUrl = ko.observable(imageUrl);
 	}
 	
-	self.artists = ko.observableArray();
+	self.searchResult = ko.observableArray();
+	self.searchBoxVal = ko.observable();
 
-	self.artistSearch = ko.observable("");
-
-
-	self.searchArtist = ko.computed(function(){
-		self.artists([]);
-
-		$.get(`spotify/get-artists-from-name?artistName=${self.artistSearch()}`, function(data) {
-			// "Data" is the object we get from the API. See server.js for the function that returns it.
-			console.group('%cResponse from /get-artist-from-name', 'color: #F037A5; font-size: large');
-			console.log(data);
-			console.groupEnd();
-			
-			data.artist.forEach( artist => {
-				if (artist.image.length > 0) {
-					self.artists.push(new Artist(artist.id, artist.name, artist.image[1].url));
-				} else {
-					self.artists.push(new Artist(artist.id, artist.name, "https://upload.wikimedia.org/wikipedia/commons/5/5f/Grey.PNG"));
-				}
+	self.searchArtists = ko.pureComputed({
+		read: function () {
+			//self.searchResult([]);
+			return self.searchResult(); // 'fetched' from an array.
+		},
+		write: function (value) {
+			$.get(`spotify/get-artists-from-name?artistName=${self.searchBoxVal()}`, function(data) {
+				// "Data" is the object we get from the API. See server.js for the function that returns it.
+				console.group('%cResponse from /get-artist-from-name', 'color: #F037A5; font-size: large');
+				console.table(data);
+				console.groupEnd();
+				data.artist.forEach( artist => {
+					self.searchResult().push(new Artist(artist.id, artist.name, artist.image.url));
+				});
 			});
-		});
-	}, this);
+		},
+		owner: self
+	});
 }
 
 ko.applyBindings(new searchModel());
