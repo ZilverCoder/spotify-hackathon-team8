6354760af6a5c62031a3eaa42d691eb0edc126a3
op3003 <rikard.helgegren@gmail.com>
2018-10-13 14:50:47 +0200

    input of artist

--- controllers/SpotifyController.js
+++ controllers/SpotifyController.js
@@ -29,89 +29,27 @@ var app = express();
 //------------------------- API CALLS -------------------------//
 //-------------------------------------------------------------//
 
-app.get('/spotify/get-album-data-for-artist', function (request, response) {
-	var artistId = request.query.artistId;
-	console.log("Hello there");
-	console.log("General Kenobi");
-	spotifyApi.getArtistAlbums(artist)
-  .then(function(data) {
-		console.log(data);
-  }, function(err) {
-    console.error(err);
-  });
-});
-/*
-app.get('/search-track', function (request, response) {
-	var track = request.query.track;
 
-	// Search for a track!
-	spotifyApi.searchTracks(`track:${track}`, {limit: 5})
-	  .then(function(data) {
-	  
-		// Send the first (only) track object
-		response.send(data.body.tracks.items);
+// Search artists whose name contains 'Love'
+app.get('/get-artists-from-name' , function (request, response) {
+	var artistName = request.query.artistName;
+	response = {};
+	spotifyApi.searchArtists(artistName)
+		.then(function(data) {
+			response.artist = [];
+			var items = data.body.artists.items;
 
-		//response.redirect('/');
-	  }, function(err) {
+      // loop through top artists
+			for (var i = 0; i < items.length ; i++) {
+				response.artist[i] = {"name" : items[i].name, "image" : items[i].images}
+				var nrShownItems = 2;
+				if (i === nrShownItems) { break; }
+				// more statements
+			 }
+		}, function(err) {
 			console.error(err);
-	  });
+		}
+		);
 });
-  
-app.get('/category-playlists', function (request, response) {
-	
-	// Get playlists from a browse category
-	// Find out which categories are available here: https://beta.developer.spotify.com/console/get-browse-categories/
-	spotifyApi.getPlaylistsForCategory('jazz', { limit : 5 })
-	  .then(function(data) {
-	  
-	  // Send the list of playlists
-	  response.send(data.body.playlists);
-	  
-	}, function(err) {
-	  console.error(err);
-	});
-});
-  
-  app.get('/audio-features', function (request, response) {
-	
-	// Get the audio features for a track ID
-	spotifyApi.getAudioFeaturesForTrack('1zP26xND9zCLGZt9NaamfL')
-	  .then(function(data) {
-	  
-		//Send the audio features object
-		response.send(data.body);
-	  
-	  }, function(err) {
-		console.error(err);
-	  });
-  });
-  
-  app.get('/artist', function (request, response) {
-	
-	// Get information about an artist
-	spotifyApi.getArtist('6jJ0s89eD6GaHleKKya26X')
-	  .then(function(data) {
-	  
-		// Send the list of tracks
-		response.send(data.body);
-	  
-	  }, function(err) {
-		console.error(err);
-	  });
-  });
-  
-  app.get('/artist-top-tracks', function (request, response) {
-	
-	// Get an artist's top tracks in a country
-	spotifyApi.getArtistTopTracks('5dHtLfkZTRquQ8tyo4ueMC', 'TW')
-	  .then(function(data) {
-	  
-		// Send the list of tracks
-		response.send(data.body.tracks);
-	  
-	  }, function(err) {
-		console.error(err);
-	  });
-  });*/
   
 module.exports = app;
--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,12 +4,15 @@
 $(function() {
 		
 		
-	
-        $.get('spotify/get-album-data-for-artist?artist=0oSGxfWSnnOXhD2fKuz2Gy', function(data) {
-        // "Data" is the object we get from the API. See server.js for the function that returns it.
-        console.group('%cResponse from /spotify/get-album-data-for-artist', 'color: #F037A5; font-size: large');
-        console.log(data);
-        console.groupEnd();
-        });
-	  
+        //Make a new call 
+        $.get('spotify/get-artists-from-name?artistName=metallica', function(data) {
+                // "Data" is the object we get from the API. See server.js for the function that returns it.
+                console.group('%cResponse from /spotify/get-artists-from-name', 'color: #F037A5; font-size: large');
+                console.log(data);
+                console.groupEnd();
+                });
+                  
+
+
+  
 });
