0b3f652eafd4ac5637a551e51c9efa665d277af8
ZilverCoder <zilvergamer@gmail.com>
2018-10-13 17:20:15 +0200

    Chart grabs real data

--- public/scripts/searchModel.js
+++ public/scripts/searchModel.js
@@ -9,12 +9,16 @@ var searchModel = function(){
 	self.trackSearch = ko.observable();
 	self.searchTrack = ko.computed(function(){
 		self.tracks([]);
-		$.get(`spotify/get-album-data-for-artist?artist=0oSGxfWSnnOXhD2fKuz2Gy`, function(data) {
+		/* $.get(`spotify/get-album-data-for-artist?artistId=0oSGxfWSnnOXhD2fKuz2Gy`, function(data) {
 			// "Data" is the object we get from the API. See server.js for the function that returns it.
 			console.group('%cResponse from /search-track', 'color: #F037A5; font-size: large');
 			console.log(data);
 			console.groupEnd();
-		});
+			$.each(data, function(index, track){
+				//console.log(track.name, track.images[1]);
+				self.tracks().push(new Track(track.name, track.images[1].url));
+			});
+		}); */
 		return "done";
 	}, this);
 }
